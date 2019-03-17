7fe0855f930cf0611445059171e0cc72f186a8c8
Dan Mallott <DKMallott@hotmail.com>
2019-03-17 13:05:04 -0500

    Add enable CLR

---
+++ RefereeSchedulingSystem.Database.Tests/Predeploy/EnableCLR.sql
@@ -0,0 +1,13 @@
+﻿EXEC sp_configure
+     'show advanced options',
+     1;
+RECONFIGURE;
+GO
+EXEC sp_configure
+     'clr enabled',
+     1;
+EXEC sp_configure
+     'clr strict security',
+     0;
+RECONFIGURE;
+GO
