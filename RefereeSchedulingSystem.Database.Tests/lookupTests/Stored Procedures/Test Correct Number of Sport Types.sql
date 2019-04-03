9d521ad1459ec5427298d2af6d84b818cea90ef2
Dan Mallott <DKMallott@hotmail.com>
2019-04-02 23:01:27 -0400

    Adding a test

---
+++ RefereeSchedulingSystem.Database.Tests/lookupTests/Stored Procedures/Test Correct Number of Sport Types.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE lookupTests.[Test Correct Number of Sport Types]
+AS
+BEGIN
+    DECLARE @countOfTypes INT;
+    DECLARE @expected INT;
+    SET @expected = 1;
+
+    SET @countOfTypes = (SELECT COUNT(*) FROM [Lookup].SportType);
+
+    DECLARE @message NVARCHAR(MAX);
+    SET @message = N'Incorrect number of Sport Types found. Expected: ' + CAST(@expected AS NVARCHAR(2)) + N'; Found: ' + CAST(@countOfTypes AS NVARCHAR(2));
+
+    EXEC tSQLt.AssertEquals @Expected = @expected, @Actual = @countOfTypes, @Message = @message;
+END
